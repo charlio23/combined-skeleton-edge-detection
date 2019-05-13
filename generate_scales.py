ded3a1e2bf82080903b092ed5b20d2b4f94b400c
charlio23 <carlesbalsellsrodas@hotmail.com>
2019-05-13 04:12:26 -0400

    scale generator

---
+++ generate_scales.py
@@ -0,0 +1,27 @@
+import os
+from PIL import Image
+import numpy as np
+from scipy.ndimage.morphology import distance_transform_edt as bwdist
+from tqdm import tqdm
+from matplotlib import pyplot as plt
+
+rootDir = "../train2017/groundTruth"
+
+edgeDir = rootDir + "/edges/"
+skeletonDir = rootDir + "/skeletons/"
+scaleDir = rootDir + "/scale/"
+os.makedirs(scaleDir, exist_ok=True)
+
+fileList = os.listdir(edgeDir)
+
+for fileName in tqdm(fileList):
+    edge = (np.array(Image.open(edgeDir + fileName).convert('L')) > 200).astype(float)
+    skeleton = (np.array(Image.open(skeletonDir + fileName).convert('L')) > 200).astype(float)
+    dist = 2.0*bwdist(1.0 - edge)
+    make_scale = np.vectorize(lambda x, y: 0 if y < 0.99 else x)
+
+    scale = make_scale(dist, skeleton).astype(np.uint8)
+
+    fileName = fileName.replace('.jpg','.png')
+    
+    Image.fromarray(scale, 'L').save(scaleDir + fileName)
