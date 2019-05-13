3e4bb6152d22a9e833d87bd790544c4246880ebe
charlio23 <carlesbalsellsrodas@hotmail.com>
2019-05-13 08:30:15 -0400

    generate scales for coco

--- generate_scales.py
+++ generate_scales.py
@@ -21,9 +21,10 @@ for fileName in tqdm(fileList):
     make_scale = np.vectorize(lambda x, y: 0 if y < 0.99 else x)
 
     scale = make_scale(dist, skeleton).astype(np.uint8)
-
+    edge = (edge*255.0).astype(np.uint8)
+    skeleton = (skeleton*255.0).astype(np.uint8)
     fileName = fileName.replace('.jpg','.png')
     
-    Image.fromarray(edge.astype(np.uint8), 'L').save(edgeDir + fileName)
-    Image.fromarray(skeleton.astype(np.uint8), 'L').save(skeletonDir + fileName)
+    Image.fromarray(edge, 'L').save(edgeDir + fileName)
+    Image.fromarray(skeleton, 'L').save(skeletonDir + fileName)
     Image.fromarray(scale, 'L').save(scaleDir + fileName)
