284b3737daf4649889544f0809f3bba9a9b9137b
charlio23 <carlesbalsellsrodas@hotmail.com>
2019-05-30 12:32:10 -0400

    changed nms calculation

--- nonmaxsuppression.py
+++ nonmaxsuppression.py
@@ -6,8 +6,8 @@ def sobel_filters(img):
     Kx = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]], np.float32)
     Ky = np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]], np.float32)
     
-    Kx = torch.from_numpy(np.ascontiguousarray(Kx[::-1, ::-1])).cuda().unsqueeze_(0).unsqueeze_(0)
-    Ky = torch.from_numpy(np.ascontiguousarray(Ky[::-1, ::-1])).cuda().unsqueeze_(0).unsqueeze_(0)
+    Kx = torch.from_numpy(np.ascontiguousarray(Kx[::-1, ::-1])).unsqueeze_(0).unsqueeze_(0)
+    Ky = torch.from_numpy(np.ascontiguousarray(Ky[::-1, ::-1])).unsqueeze_(0).unsqueeze_(0)
 
     Ix = torch.nn.functional.conv2d(img, Kx, padding=1)
     Iy = torch.nn.functional.conv2d(img, Ky, padding=1)
@@ -18,52 +18,39 @@ def sobel_filters(img):
     
     return Ix, Iy
 
-def interp(img, h, w, x , y):
-    if x < 0:
-        x = 0
-    elif x > h - 1.001:
-        x = h - 1.001
-    if y < 0:
-        y = 0
-    elif y > w - 1.001:
-        y = w - 1.001
-    x0, y0 = int(x), int(y)
-    x1, y1 = x0 + 1, y0 + 1
-    dx0, dy0 = float(x) - float(x0), float(y) - float(y0)
-    dx1, dy1 = 1 - dx0, 1 - dy0
-    return img[x0,y0]*dx1*dy1 + img[x1,y0]*dx0*dy1 + img[x0,y1]*dx1*dy0 + img[x1,y1]*dx0*dy0;
-
-
-def nonmax(img, O, r, s, m):
+def nonmax(img, O, r, m):
     h, w = img.shape
-    edge = torch.zeros((h,w))
-    for i in range(h):
-        for j in range(w):
-            e = edge[i,j] = img[i,j]
-            if (e < 1e-4):
-                continue
-            e =  e*m
-            coso, sino = np.cos(O[i,j].item()), np.sin(O[i,j].item())
-            for d in range(-r, r+1):
-                if d:
-                    e0 = interp(img, h, w, i+d*coso, j+d*sino)
-                    if e < e0:
-                        edge[i,j] = 0
-                        break
+    edge = img.clone()
+    e = img.clone()*m
+    iMat = torch.t(torch.arange(h).float()*torch.ones((w,1)))
+    jMat = torch.arange(w).float()*torch.ones((h,1))
+    index = torch.cat([iMat.unsqueeze(-1), jMat.unsqueeze(-1)], -1)
+    cosO, sinO = torch.cos(O.float()), torch.sin(O.float())
+    angle = torch.cat([cosO.unsqueeze(-1), sinO.unsqueeze(-1)], -1)
+    for d in r:
+        ort = index + d*angle
+        xy = torch.cat([ort[:,:,:1].clamp(0,h - 1.001),ort[:,:,1:].clamp(0,w - 1.001)], -1)
+        xy0 = xy.long()
+        xy1 = xy0.add(1)
+        dxy0 = xy - xy0.float()
+        dxy1 = 1 - dxy0
+        x1y0 = torch.cat([xy1[:,:,:1], xy0[:,:,1:]], -1)
+        x0y1 = torch.cat([xy0[:,:,:1], xy1[:,:,1:]], -1)
+        res1 = img[xy0.reshape(-1,2).t().chunk(chunks=2,dim=0)].reshape(h,w)*dxy1[:,:,0]*dxy1[:,:,1]
+        res2 = img[x1y0.reshape(-1,2).t().chunk(chunks=2,dim=0)].reshape(h,w)*dxy0[:,:,0]*dxy1[:,:,1]
+        res3 = img[x0y1.reshape(-1,2).t().chunk(chunks=2,dim=0)].reshape(h,w)*dxy1[:,:,0]*dxy0[:,:,1]
+        res4 = img[xy1.reshape(-1,2).t().chunk(chunks=2,dim=0)].reshape(h,w)*dxy0[:,:,0]*dxy0[:,:,1]
+        e0 = res1 + res2 + res3 + res4
+        edge[(e<e0)] = 0
     return edge
 
-
-
 def nms(img):
-    print("first sobel")
+    img = img.unsqueeze_(0).float()
     Ox, Oy = sobel_filters(img)
-    print("second sobel")
     Oxx, _ = sobel_filters(Ox)
-    print("third sobel")
     Oxy, Oyy = sobel_filters(Oy)
-    print("O")
     O = torch.remainder(torch.atan(torch.div(torch.mul(Oyy, torch.sign(-Oxy)),(Oxx + 1e-5))), np.pi).squeeze_(0).squeeze_(0).to(torch.uint8)
-    print("nonmax")
-    E = nonmax(img[0][0], O, 1, 5, 1.01)
+    img = img.squeeze_(0).squeeze_(0)
+    E = nonmax(img, O, [-1, 1], 1.01)
 
     return E.unsqueeze_(0).unsqueeze_(0)
